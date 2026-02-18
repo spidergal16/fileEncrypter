b9767fa4a904aa1690dbbba62536464f6b964783
unknown <spidergal2009@gmail.com>
2026-02-18 19:45:10 +0200

    feat: improve main

--- File_Encrypt_Decrypt/main.cpp
+++ File_Encrypt_Decrypt/main.cpp
@@ -6,12 +6,16 @@
 
 int main()
 {
+	std::string key = "";
 	std::string filePath = "";
 
 	std::cout << "Enter the file path to encrypt: " << std::endl;
-	std::cin >> filePath;	
+	std::cin >> filePath;
 
-	Decrypt::decryptFile(Encrypt::encryptFile(filePath, "", "superSECRETkey"), "", "superSECRETkey");
+	std::cout << "Enter the key to encrypt with: " << std::endl;
+	std::cin >> key;
+
+	Decrypt::decryptFile(Encrypt::encryptFile(filePath, "", key), "", key);
 
 	return 0;
 }
