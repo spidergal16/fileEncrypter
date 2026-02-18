93c2a7b79f1bb4434ea5f67ef290fdb3ead3ceaa
unknown <spidergal2009@gmail.com>
2026-02-18 20:13:36 +0200

    feat: add option to save the key in a file

--- File_Encrypt_Decrypt/main.cpp
+++ File_Encrypt_Decrypt/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include "Encrypt.h"
@@ -8,14 +9,25 @@ int main()
 {
 	std::string key = "";
 	std::string filePath = "";
+	int saveKey = 0;
 
 	std::cout << "Enter the file path to encrypt: " << std::endl;
 	std::cin >> filePath;
 
-	std::cout << "Enter the key to encrypt with: " << std::endl;
-	std::cin >> key;
+	std::cout << "Enter the key to encrypt with (no spaces): " << std::endl;
+	std::cin >> key;	
+
+	std::cout << "Do you want to save the key into a file? (0 - no, other number - yes): " << std::endl;	
+	std::cin >> saveKey;
 
 	Decrypt::decryptFile(Encrypt::encryptFile(filePath, "", key), "", key);
+
+	if (saveKey)
+	{
+		std::ofstream keyFile("key.txt");
+		keyFile << key;
+		keyFile.close();
+	}
 
 	return 0;
 }
